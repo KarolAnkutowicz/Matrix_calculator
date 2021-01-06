aca21a2fae9367893b82651005b2fa4cf2e5cea2
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-01-06 22:14:49 +0100

    Comments constantsandtypes.h
    Expansion description in file constantsandtypes.h.

--- Matrix_calculator/constantsandtypes.h
+++ Matrix_calculator/constantsandtypes.h
@@ -6,20 +6,37 @@
 
 using namespace std;
 
+/*
+ * unsigned short typeSize - zdefiniowanie synonimu typu dla rozmiaru
+ */
 typedef unsigned short typeSize;
-
+/*
+ * char operatorScalarProduct - zdefiniowanie operatora iloczynu skalarnego wektorow
+ */
 const char operatorScalarProduct = 'o';
-
+/*
+ * char operatorCrossProduct - zdefiniowanie operatora iloczynu wektorowego wektorow
+ */
 const char operatorCrossProduct = 'x';
-
+/*
+ * char operatorLengthVector - zdefiniowanie operatora dlugosci wektora
+ */
 const char operatorLengthVector = 'L';
-
+/*
+ * char operatorDeterminant - zdefiniowanie operatora obliczania wyznacznika macierzy
+ */
 const char operatorDeterminant = 'D';
-
+/*
+ * char operatorReversal - zdefiniowanie operatora wyznaczania macierzy odwrotnej
+ */
 const char operatorReversal = '@';
-
+/*
+ * char printHelp - zdefiniowanie operatora wyswietlania pomocy
+ */
 const char printHelp = '?';
-
+/*
+ * char endOfProgram1, char endOfProgram2 - zdefiniowanie operatorow zakonczenia dzialania programu
+ */
 const char endOfProgram1 = 'k';
 const char endOfProgram2 = 'K';
 
