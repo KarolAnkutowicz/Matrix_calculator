b72b18293956768c91808bdac44bb037c78b37f9
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-01-08 20:43:12 +0100

    Expansion cmatrix.h

--- Matrix_calculator/cmatrix.cpp
+++ Matrix_calculator/cmatrix.cpp
@@ -5,9 +5,234 @@
 
 using namespace std;
 
+/*
+ * cMatrix()
+ */
 cMatrix::cMatrix()
 {
+
 }
+/*
+ * cMatrix(typeSize parRows, typeSize parColumns)
+ */
+/*cMatrix::cMatrix(typeSize parRows, typeSize parColumns)
+{
+
+}*/
+/*
+ * cMatrix(typeSize parRows, typeSize parColumns, double *parTabElements)
+ */
+/*cMatrix::cMatrix(typeSize parRows, typeSize parColumns, double *parTabElements)
+{
+
+}*/
+/*
+ * cMatrix(const cMatrix &M)
+ */
+/*cMatrix::cMatrix(const cMatrix &M)
+{
+
+}*/
+/*
+ * ~cMatrix()
+ */
+/*cMatrix::~cMatrix()
+{
+
+}*/
+
+/*
+ * void mClearElements()
+ */
+void cMatrix::mClearElements()
+{
+
+}
+
+/*
+ * void mTestMatrixZeros()
+ */
+void cMatrix::mTestMatrixZeros()
+{
+
+}
+
+/*
+ * void mTestMatrixIdentity()
+ */
+void cMatrix::mTestMatrixIdentity()
+{
+
+}
+/*
+ * void mTestMatrixDiagonal()
+ */
+void cMatrix::mTestMatrixDiagonal()
+{
+
+}
+/*
+ * void mTestMatrixTriangularUpper()
+ */
+void cMatrix::mTestMatrixTriangularUpper()
+{
+
+}
+/*
+ * void mTestMatrixTraingularLower()
+ */
+void cMatrix::mTestMatrixTraingularLower()
+{
+
+}
+/*
+ * void mTests()
+ */
+void cMatrix::mTests()
+{
+
+}
+
+/*
+ * ostream &operator << (ostream &streamOut, cMatrix &M)
+ */
+/*ostream &operator << (ostream &streamOut, cMatrix &M)
+{
+
+}*/
+/*
+ * istream &operator >> (istream & strIn, cMatrix &M)
+ */
+/*istream &operator >> (istream & strIn, cMatrix &M)
+{
+
+}*/
+
+/*
+ * cMatrix &operator = (const cMatrix &M)
+ */
+/*cMatrix &cMatrix::operator = (const cMatrix &M)
+{
+
+}*/
+
+/*
+ * cMatrix operator + (cMatrix M)
+ */
+/*cMatrix cMatrix::operator + (cMatrix M)
+{
+
+}*/
+/*
+ * cMatrix operator - (cMatrix M)
+ */
+/*cMatrix cMatrix::operator - (cMatrix M)
+{
+
+}*/
+/*
+ * cMatrix operator * (double parFactor)
+ */
+/*cMatrix cMatrix::operator * (double parFactor)
+{
+
+}*/
+/*
+ * cMatrix operator * (cMatrix M)
+ */
+/*cMatrix operator * (cMatrix M)
+{
+
+}*/
+
+/*
+ * double mCalculateDeterminant2x2()
+ */
+/*double cMatrix::mCalculateDeterminant2x2()
+{
+
+}*/
+/*
+ * double mCalculateDeterminant3x3()
+ */
+/*double cMatrix::mCalculateDeterminant3x3()
+{
+
+}*/
+/*
+ * double mCalculateDeterminantDiagonal()
+ */
+/*double cMatrix::mCalculateDeterminantDiagonal()
+{
+
+}*/
+/*
+ * double mCalculateDeterminant()
+ */
+/*double cMatrix::mCalculateDeterminant()
+{
+
+}*/
+
+/*
+ * cMatrix mExponentiationMatrix(typeSize parPower)
+ */
+/*cMatrix cMatrix::mExponentiationMatrix(typeSize parPower)
+{
+
+}*/
+
+/*
+ * cMatrix mInversalMatrix1x1()
+ */
+/*cMatrix cMatrix::mInversalMatrix1x1()
+{
+
+}*/
+/*
+ * cMatrix mInversalMatrix2x2()
+ */
+/*cMatrix cMatrix::mInversalMatrix2x2()
+{
+
+}*/
+/*
+ * cMatrix mInversalMatrix3x3()
+ */
+/*cMatrix cMatrix::mInversalMatrix3x3()
+{
+
+}*/
+/*
+ * cMatrix mInversalMatrix()
+ */
+/*cMatrix cMatrix::mInversalMatrix()
+{
+
+}*/
+
+/*
+ * double mScalarProduct(cMatrix M)
+ */
+/*double cMatrix::mScalarProduct(cMatrix M)
+{
+
+}*/
+/*
+ * cMatrix mCrossProduct(cMatrix M)
+ */
+/*cMatrix cMatrix::mCrossProduct(cMatrix M)
+{
+
+}*/
+/*
+ * double mLengthVector()
+ */
+/*double cMatrix::mLengthVector()
+{
+
+}*/
+
 
 /* cmatrix.cpp */
 /********** END_OF_FILE **********/
