4d4640bddbf1e65edaebbc8952072f8461986e70
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-04-13 23:35:28 +0200

    Fields in cMatrix
    Add fields in cmatrix class.

--- Matrix_calculator/cmatrix.h
+++ Matrix_calculator/cmatrix.h
@@ -4,12 +4,65 @@
 #ifndef CMATRIX_H
 #define CMATRIX_H
 
+#include "constantsandtypes.h"
+
 using namespace std;
 
 class cMatrix
 {
 /********** PRIVATE: BEGINNING **********/
 
+    /*
+     * vRows, vColumns - pola przechowujace wymiary macierzy
+     */
+    typeSize vRows, vColumns;
+
+    /*
+     * *tableElements - tablica elementow macierzy
+     */
+    double *tableElements;
+
+    /*
+     * vIfVector - pole przechowujaca informacje czy macierz
+     * jest wektorem
+     */
+    bool vIfVector;
+
+    /*
+     * vIfMatrixZeros - pole przechowujace informacje czy macierz
+     * jest macierza zerowa
+     */
+    bool vIfMatrixZeros;
+
+    /*
+     * vIfMatrixSquare - pole przechowujace informacje czy macierz
+     * jest kwadratowa
+     */
+    bool vIfMatrixSquare;
+
+    /*
+     * vIfMatrixIdentity - pole przechowujace informacje czy macierz
+     * jest jednostkowa
+     */
+    bool vIfMatrixIdentity;
+
+    /*
+     * vIfMatrixDiagonal - pole przechowujace informacje czy macierz
+     * jest diagonalna
+     */
+    bool vIfMatrixDiagonal;
+
+    /*
+     * vIfMatrixTriangularUpper - pole przechowujace informacje czy
+     * macierz jest macierza trojkatna gorna
+     */
+    bool vIfMatrixTriangularUpper;
+
+    /*
+     * vIfMatrixTriangularLower - pole przechowujace informacje czy
+     * macierz jest macierza trojkatna dolna
+     */
+    bool vIfMatrixTriangularLower;
 
 /********** PRIVATE: END **********/
 
