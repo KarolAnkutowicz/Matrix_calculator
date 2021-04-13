7dd1bcfdbc32ed222774b64c3b1170f25e5de261
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-04-13 22:43:13 +0200

    Constants and types - operators
    Add definitions of operators in constantsandtypes.h

--- Matrix_calculator/constantsandtypes.h
+++ Matrix_calculator/constantsandtypes.h
@@ -6,6 +6,50 @@
 
 using namespace std;
 
+/*
+ * unsigned short typeSize - zdefiniowanie synonimu typu dla rozmiaru
+ */
+typedef unsigned short typeSize;
+
+/*
+ * char operatorScalarProduct - zdefiniowanie operatora iloczynu skalarnego
+ */
+const char operatorScalarProduct = 'o';
+
+/*
+ * char operatorCrossProduct - zdefiniowanie operatora iloczynu wektorowego
+ */
+const char operatorCrossProduct = 'x';
+
+/*
+ * char operatorLengthVector - zdefiniowanie operatora dlugosci wektora
+ */
+const char operatorLengthVector = 'L';
+
+/*
+ * char operatorDeterminant - zdefiniowanie operatora obliczania wyznacznika macierzy
+ */
+const char operatorDeterminant = 'D';
+
+/*
+ * char operatorReversal - zdefiniowanie operatora  wyznaczania macierzy odwrotnej
+ */
+const char operatorReversal = '@';
+
+/*
+ * char operatorExponentiation - zdefiniowanie operatora potegowania macierzy
+ */
+const char operatorExponentiation = '^';
+
+/*
+ * char operatorTransposition - zdefiniowanie operatora transponowania macierzy
+ */
+const char operatorTransposition = 'T';
+
+/*
+ * char endOfProgram - zdefiniowanie operatora zakonczenia programu
+ */
+const char endOfProgram = 'k';
 
 #endif // CONSTANTSANDTYPES_H
 
