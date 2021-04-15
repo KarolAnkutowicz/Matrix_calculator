8f620e8f2b497e846027051b8d69f5a256784d29
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-04-15 22:30:19 +0200

    Methods of calculating determinant
    Add headerers of definitions methods if calculating determinant in cMatrix class.

--- Matrix_calculator/cmatrix.h
+++ Matrix_calculator/cmatrix.h
@@ -504,6 +504,47 @@ public:
 
 
 
+    /*
+     * double mCalculateDeterminant2x2() - metoda obliczajaca
+     * wyznacznik macierzy o wymiarze 2.
+     * PRE:
+     * - brak.
+     * POST:
+     * - zwrocenie wyniku (typ: double).
+     */
+    double mCalculateDeterminant2x2();
+
+    /*
+     * double mCalculateDeterminant3x3() - metoda obliczajaca
+     * wyznacznik macierzy o wymiarze 3.
+     * PRE:
+     * - brak.
+     * POST:
+     * - zwrocenie wyniku (typ: double).
+     */
+    double mCalculateDeterminant3x3();
+
+    /*
+     * double mCalculateDeterminantDiagonal() - metoda obliczajaca
+     * wyznacznik macierzy diagonalnej o dowolnym wymiarze wiekszym
+     * od 3.
+     * PRE:
+     * - brak.
+     * POST:
+     * - zwrocenie wyniku (typ: double).
+     */
+    double mCalculateDeterminantDiagonal();
+
+    /*
+     * double mCalculateDeterminant() - metoda obliczajaca
+     * wyznacznik macierzy o dowolnym wymiarze wiekszym od 3.
+     * PRE:
+     * - brak.
+     * POST:
+     * - zwrocenie wyniku (typ: double).
+     */
+    double mCalculateDeterminant();
+
 
 /********** PUBLIC: END **********/
 };
