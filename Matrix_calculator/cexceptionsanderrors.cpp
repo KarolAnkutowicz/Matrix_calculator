1d4ef1898416add647b821a158d4b6d98375b1d6
KarolAnkutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2021-05-31 10:51:21 +0200

    Expansion cExceptionsAndErrors class
    Add declaration new method in cAxceptionsAndErrors class.

--- Matrix_calculator/cexceptionsanderrors.cpp
+++ Matrix_calculator/cexceptionsanderrors.cpp
@@ -1,5 +1,31 @@
+/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * file: cexceptionsanderrors.cpp
+ */
+
 #include "cexceptionsanderrors.h"
 
+using namespace std;
+
+/********** PUBLIC: BEGINNING **********/
+
+/*
+ * cExceptionsAndErrors()
+ */
 cExceptionsAndErrors::cExceptionsAndErrors()
 {
 }
+
+/*
+ * void mDisplayMessage(typeSize pNumber)
+ */
+void cExceptionsAndErrors::mDisplayMessage(typeSize pNumber)
+{
+}
+
+
+/********** PUBLIC: END **********/
+
+/* cexceptionsanderrors.cpp */
+/********** END_OF_FILE **********/
